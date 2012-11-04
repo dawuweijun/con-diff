22709ce5414c7b5bc4a75ff2ca0e92259d90c854
Black <1507912984@qq.com>
2012-11-04 11:26:05 +0800

    重新编写tdms算法

---
+++ DiagMatEquationSolve.f95
@@ -0,0 +1,25 @@
+module DMESolve
+use Matrix
+contains
+!五对角和三对角矩阵方程的求解
+function DMEResolve(in_mat,in_right)
+	real,dimension(:),pointer::DMEResolve
+	real,dimension(:),pointer::in_right
+	type(DiagMatrix)::in_mat
+	real::temp
+!直接对矩阵进行操作，不再复制
+!五对角矩阵，化简为三对角矩阵
+if(in_mat%MatType=5)then
+!第一次迭代，消除WW
+do I=3,MaxSize
+	temp=in_mat%left_WW(I)/in_mat%left_W(I-1)
+	in_mat%left_W(I)=in_mat%left_W(I)-in_mat%left_P(I-1)*temp
+	in_mat%left_P(I)=in_mat%left_P(I)-in_mat%left_E(I-1)*temp
+	!注意，in_mat%left_E（MaxSize）应该为零，但是无所谓
+	in_mat%left_E(I)=in_mat%left_E(I)-in_mat%left_EE(I-1)*temp
+	in_right(I)=in_right(I)-in_right(I-1)*temp
+end do
+!第二次迭代，消除EE
+end if
+end function
+end module
